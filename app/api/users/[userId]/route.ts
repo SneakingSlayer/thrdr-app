f3e8b64fd7a9cee518fcae099d485e1f096443a8
RHLance <lance.endaya@rewardholdings.com>
2023-06-26 20:03:31 +0800

    feat: implement edit user profile

--- app/api/users/[userId]/route.ts
+++ app/api/users/[userId]/route.ts
@@ -1,7 +1,9 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/prisma/utils";
+import { verifyToken } from "@/utils";
+import bcrypt from "bcrypt";
 export const GET = async (
-  req: Response,
+  req: NextRequest,
   { params }: { params: { userId: string } }
 ) => {
   try {
@@ -22,5 +24,38 @@ export const GET = async (
     return NextResponse.json({ data: getUser });
   } catch (error) {
     return error;
+  }
+};
+
+export const PUT = async (
+  req: NextRequest,
+  { params }: { params: { userId: string } }
+) => {
+  try {
+    const token = await verifyToken(req);
+    if (!token.isValid) throw { ...token };
+    const reqBody = await req.json();
+    console.log(token);
+    if (token.token?.id !== params.userId)
+      throw { ...token, message: "Accounts does not match!" };
+    const salt = await bcrypt.genSalt(10);
+    const hashPass = await bcrypt.hash(reqBody.password, salt);
+    if (!reqBody.password) delete reqBody.password;
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: params.userId,
+      },
+      data: !reqBody.password
+        ? {
+            ...reqBody,
+          }
+        : { ...reqBody, password: hashPass },
+    });
+    return NextResponse.json({ data: updatedUser });
+  } catch (error: any) {
+    return NextResponse.json(
+      { data: error?.message ?? "Bad request" },
+      { status: error?.status ?? 400 }
+    );
   }
 };
--- components/profile/ProfileForm.tsx
+++ components/profile/ProfileForm.tsx
@@ -6,6 +6,7 @@ import {
   Box,
   Button,
   ButtonGroup,
+  Flex,
   FormControl,
   FormLabel,
   Input,
@@ -16,10 +17,16 @@ import { User } from "@/types";
 
 import { useForm } from "react-hook-form";
 
-const ProfileForm = (props: User) => {
-  const { name, email, userName } = props;
+import { useMutation } from "@tanstack/react-query";
+import { USERS_API } from "@/constants";
+import { useRouter } from "next/navigation";
 
-  const { register } = useForm({
+const ProfileForm = (props: User) => {
+  const { name, email, userName, id } = props;
+
+  const router = useRouter();
+
+  const { register, handleSubmit, getValues } = useForm({
     defaultValues: {
       name,
       email,
@@ -27,62 +34,82 @@ const ProfileForm = (props: User) => {
       password: "",
     },
   });
+
+  const { mutate, isLoading, isSuccess } = useMutation({
+    mutationFn: async (newValue) => {
+      const result = await fetch(`${USERS_API}/${id}`, {
+        method: "PUT",
+        body: JSON.stringify(newValue),
+      });
+      const res = await result.json();
+      return res;
+    },
+  });
+
+  const onSubmit = async (values: any) => await mutate(values);
+
+  React.useEffect(() => {
+    if (isSuccess) router.push(`/me/${getValues("userName")}`);
+  }, [getValues, isSuccess, router]);
+
   return (
-    <Box as="form">
-      <VStack spacing={4}>
-        <FormControl>
-          <FormLabel fontSize={"xs"} color={"gray.500"}>
-            Name
-          </FormLabel>
-          <Input
-            {...register("name")}
-            variant={"filled"}
-            size={"sm"}
-            placeholder="Name"
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel fontSize={"xs"} color={"gray.500"}>
-            Email
-          </FormLabel>
-          <Input
-            {...register("email")}
-            variant={"filled"}
-            size={"sm"}
-            placeholder="Email"
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel fontSize={"xs"} color={"gray.500"}>
-            Username
-          </FormLabel>
-          <Input
-            {...register("userName")}
-            variant={"filled"}
-            size={"sm"}
-            placeholder="Username"
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel fontSize={"xs"} color={"gray.500"}>
-            Password
-          </FormLabel>
-          <Input
-            type="password"
-            {...register("password")}
-            variant={"filled"}
-            size={"sm"}
-            placeholder="Password"
-          />
-        </FormControl>
-      </VStack>
-      <ButtonGroup mt={4} w={"100%"} justifyContent={"flex-end"}>
-        <Button variant={"ghost"} size={"sm"} color={"red.400"}>
-          Delete Account
-        </Button>
-        <SubmitButton isSubmitting={false} title="Save" />
-      </ButtonGroup>
-    </Box>
+    <Flex w={"100%"} justifyContent={"center"}>
+      <Box onSubmit={handleSubmit(onSubmit)} as="form" w={"100%"} maxW={350}>
+        <VStack spacing={4}>
+          <FormControl>
+            <FormLabel fontSize={"xs"} color={"gray.500"}>
+              Name
+            </FormLabel>
+            <Input
+              {...register("name")}
+              variant={"filled"}
+              size={"sm"}
+              placeholder="Name"
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel fontSize={"xs"} color={"gray.500"}>
+              Email
+            </FormLabel>
+            <Input
+              {...register("email")}
+              variant={"filled"}
+              size={"sm"}
+              placeholder="Email"
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel fontSize={"xs"} color={"gray.500"}>
+              Username
+            </FormLabel>
+            <Input
+              {...register("userName")}
+              variant={"filled"}
+              size={"sm"}
+              placeholder="Username"
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel fontSize={"xs"} color={"gray.500"}>
+              Password
+            </FormLabel>
+            <Input
+              type="password"
+              {...register("password")}
+              variant={"filled"}
+              size={"sm"}
+              placeholder="Password"
+            />
+          </FormControl>
+        </VStack>
+        <ButtonGroup mt={4} w={"100%"} justifyContent={"flex-end"}>
+          <Button variant={"ghost"} size={"sm"} color={"red.400"}>
+            Delete Account
+          </Button>
+          <SubmitButton isSubmitting={isLoading} title="Save" />
+        </ButtonGroup>
+      </Box>
+    </Flex>
   );
 };
 
