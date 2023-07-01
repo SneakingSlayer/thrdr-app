2c592ed0032e1d46c29bf5d4f7f245844ecc3659
SneakingSlayer <endayalance@yahoo.com>
2023-07-01 22:44:19 +0800

    fix: add nullish operand to api env

--- constants/api.ts
+++ constants/api.ts
@@ -2,7 +2,7 @@ export const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
     : process.env.BASE_URL;
-export const SIGN_UP_API = `${BASE_URL}/api/auth/signup`;
-export const THREADS_API = `${BASE_URL}/api/threads`;
-export const LIKES_API = `${BASE_URL}/api/likes`;
-export const USERS_API = `${BASE_URL}/api/users`;
+export const SIGN_UP_API = `${BASE_URL ?? ""}/api/auth/signup`;
+export const THREADS_API = `${BASE_URL ?? ""}/api/threads`;
+export const LIKES_API = `${BASE_URL ?? ""}/api/likes`;
+export const USERS_API = `${BASE_URL ?? ""}/api/users`;
