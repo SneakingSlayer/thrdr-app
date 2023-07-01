7b1096f264558358ca9d29f0b7cf62e0ee5d465c
SneakingSlayer <endayalance@yahoo.com>
2023-07-01 22:50:54 +0800

    fix: incorrect likes api url

--- constants/api.ts
+++ constants/api.ts
@@ -4,5 +4,5 @@ export const BASE_URL =
     : process.env.BASE_URL;
 export const SIGN_UP_API = `${BASE_URL ?? ""}/api/auth/signup`;
 export const THREADS_API = `${BASE_URL ?? ""}/api/threads`;
-export const LIKES_API = `${BASE_URL ?? ""}/api/likes`;
+export const LIKES_API = `/likes`;
 export const USERS_API = `${BASE_URL ?? ""}/api/users`;
