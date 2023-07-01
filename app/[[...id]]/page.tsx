ae202466329fb54e4d871acc1fc6b82952cc56b5
SneakingSlayer <endayalance@yahoo.com>
2023-07-01 20:13:15 +0800

    refactor: optional catch for slug as index

--- app/[[...id]]/page.tsx
+++ app/[[...id]]/page.tsx
@@ -1,13 +1,13 @@
 import {
   ErrorPage,
+  LandingPage,
   ProfileDetails,
   ThreadForm,
   ThreadsSection,
 } from "@/components";
 import { getUserById } from "@/api";
-import { redirect } from "next/navigation";
-// params: { params: { id: string } }
 export default async function Home({ params }: { params: { id: string } }) {
+  if (!params.id) return <LandingPage />;
   const user = await getUserById(params.id);
   if (!user) return <ErrorPage />;
   return (
--- components/landing/index.ts
+++ components/landing/index.ts
@@ -5,3 +5,4 @@ export { default as Marquee } from "./Marquee";
 export { default as Banner } from "./Banner";
 export { default as Faq } from "./Faq";
 export { default as SectionContainer } from "./SectionContainer";
+export { default as LandingPage } from "./LandingPage";
