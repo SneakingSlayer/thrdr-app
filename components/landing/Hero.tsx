ee4d375e7379d05b5ff10b2959cd61c0764d914b
RHLance <lance.endaya@rewardholdings.com>
2023-07-21 20:39:13 +0800

    fix: landing page cta not working

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -5,7 +5,21 @@ import React from "react";
 import { Box, Button, ButtonGroup, Flex, Image, Text } from "@chakra-ui/react";
 import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 
+import Link from "next/link";
+
 const Hero = () => {
+  const handleScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    // first prevent the default behavior
+    e.preventDefault();
+    // get the href and remove everything before the hash (#)
+    const href = e.currentTarget.href;
+    const targetId = href.replace(/.*\#/, "");
+    // get the element by id and use scrollIntoView
+    const elem = document.getElementById(targetId);
+    elem?.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
   return (
     <Box textAlign={"center"}>
       <Box mb={6}>
@@ -20,23 +34,27 @@ const Hero = () => {
         </Text>
       </Box>
       <ButtonGroup>
-        <Button
-          bg={"brand.600"}
-          color={"white"}
-          _hover={{ bg: "brand.700" }}
-          size={"sm"}
-        >
-          Get Started
-        </Button>
-        <Button
-          color={"white"}
-          _hover={{ color: "brand.500" }}
-          size={"sm"}
-          variant={"ghost"}
-          rightIcon={<HiOutlineArrowNarrowRight fontSize={24} />}
-        >
-          Learn more
-        </Button>
+        <Link href={"/signup"}>
+          <Button
+            bg={"brand.600"}
+            color={"white"}
+            _hover={{ bg: "brand.700" }}
+            size={"sm"}
+          >
+            Get Started
+          </Button>
+        </Link>
+        <Link href="#about" onClick={handleScroll}>
+          <Button
+            color={"white"}
+            _hover={{ color: "brand.500" }}
+            size={"sm"}
+            variant={"ghost"}
+            rightIcon={<HiOutlineArrowNarrowRight fontSize={24} />}
+          >
+            Learn more
+          </Button>
+        </Link>
       </ButtonGroup>
       <Flex justifyContent={"center"} alignItems={"center"} my={10}>
         <Image w={"100%"} h={"auto"} maxW={400} src="/hero.png" alt="hero" />
