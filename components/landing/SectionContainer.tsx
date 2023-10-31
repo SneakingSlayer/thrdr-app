4e5ae81138ab529c1ec6774a360bda6472135453
SneakingSlayer <endayalance@yahoo.com>
2023-10-31 18:52:59 +0800

    fix: test webview

--- components/landing/SectionContainer.tsx
+++ components/landing/SectionContainer.tsx
@@ -7,30 +7,35 @@ import { Box } from "@chakra-ui/react";
 const SectionContainer = ({ children }: { children: React.ReactNode }) => {
   const [isWebview, setIsWebview] = React.useState(false);
 
-  var standalone = (window.navigator as any).standalone,
-    userAgent = window.navigator.userAgent.toLowerCase(),
-    safari = /safari/.test(userAgent),
-    ios = /iphone|ipod|ipad/.test(userAgent);
+  React.useEffect(() => {
+    if (typeof window !== "undefined") {
+      var standalone = (window.navigator as any).standalone,
+        userAgent = window.navigator.userAgent.toLowerCase(),
+        safari = /safari/.test(userAgent),
+        ios = /iphone|ipod|ipad/.test(userAgent);
 
-  if (ios) {
-    if (!standalone && safari) {
-      // Safari
-      console.log("WEBVIEW: false");
-    } else if (!standalone && !safari) {
-      // iOS webview
-      console.log("WEBVIEW: true");
-      setIsWebview(true);
+      if (ios) {
+        if (!standalone && safari) {
+          // Safari
+          console.log("WEBVIEW: false");
+        } else if (!standalone && !safari) {
+          // iOS webview
+          console.log("WEBVIEW: true");
+          setIsWebview(true);
+        }
+      } else {
+        if (userAgent.includes("wv")) {
+          // Android webview
+          console.log("WEBVIEW: true");
+          setIsWebview(true);
+        } else {
+          // Chrome
+          console.log("WEBVIEW: false");
+        }
+      }
     }
-  } else {
-    if (userAgent.includes("wv")) {
-      // Android webview
-      console.log("WEBVIEW: true");
-      setIsWebview(true);
-    } else {
-      // Chrome
-      console.log("WEBVIEW: false");
-    }
-  }
+  }, []);
+
   return (
     <Box mb={12}>
       {isWebview && <h1 style={{ color: "white" }}>WEBVIEW</h1>}
