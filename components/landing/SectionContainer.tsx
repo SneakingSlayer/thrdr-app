ab7ac87ce0e271bfa4fbde4e9dbddcc9dc855e91
SneakingSlayer <endayalance@yahoo.com>
2023-10-31 18:41:36 +0800

    test: check webview

--- components/landing/SectionContainer.tsx
+++ components/landing/SectionContainer.tsx
@@ -5,6 +5,28 @@ import React from "react";
 import { Box } from "@chakra-ui/react";
 
 const SectionContainer = ({ children }: { children: React.ReactNode }) => {
+  var standalone = (window.navigator as any).standalone,
+    userAgent = window.navigator.userAgent.toLowerCase(),
+    safari = /safari/.test(userAgent),
+    ios = /iphone|ipod|ipad/.test(userAgent);
+
+  if (ios) {
+    if (!standalone && safari) {
+      // Safari
+      console.log("WEBVIEW: false");
+    } else if (!standalone && !safari) {
+      // iOS webview
+      console.log("WEBVIEW: true");
+    }
+  } else {
+    if (userAgent.includes("wv")) {
+      // Android webview
+      console.log("WEBVIEW: true");
+    } else {
+      // Chrome
+      console.log("WEBVIEW: false");
+    }
+  }
   return <Box mb={12}>{children}</Box>;
 };
 
